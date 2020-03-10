52910bc867474cb500b5c0ec11f63f4f62494ef0
merkax <mishabigr@yandex.ru>
2020-03-10 15:03:37 +0300

    Remove before_action for load question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -20,10 +20,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe "GET #show" do
     before { get :show, params: { id: question } }
 
-    it "assigns the requested question to @question" do
-      expect(assigns(:question)).to eq question
-    end
-    
     it "renders show view" do
       expect(response).to render_template :show
     end
@@ -31,11 +27,7 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe "GET #new" do
     before { get :new }
-    
-    it "assigns a new Question to @question" do
-      expect(assigns(:question)).to be_a_new(Question)
-    end
-    
+
     it "renders new view" do
       expect(response).to render_template :new
     end
@@ -44,15 +36,11 @@ RSpec.describe QuestionsController, type: :controller do
   describe "GET #edit" do
     before { get :edit, params: { id: question } }
 
-    it "assigns the requested question to @question" do
-      expect(assigns(:question)).to eq question
-    end
-    
     it "renders edit view" do
       expect(response).to render_template :edit
     end
   end
-  
+
   describe "POST #create" do
     context 'with valid attributes' do
       it "saves a new question in the database" do
@@ -128,7 +116,6 @@ RSpec.describe QuestionsController, type: :controller do
         delete :destroy, params: { id: question }
         expect(response).to redirect_to questions_path
       end
-      
     end
   end
 end
