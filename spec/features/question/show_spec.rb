72dfd6946825abced79fad93d970f4009cac6f0a
merkax <mishabigr@yandex.ru>
2020-03-19 00:00:26 +0300

    add create_list for test question/show

--- spec/features/question/show_spec.rb
+++ spec/features/question/show_spec.rb
@@ -4,8 +4,7 @@ feature 'User can watch question and answers to it' do
 
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
-  given!(:answer) { create(:answer, question: question, user: user ) }
-  given!(:answer1) { create(:answer, question: question, user: user ) }
+  given!(:answer) { create_list(:answer, 2, question: question, user: user ) }
   
   describe "User" do
     background { visit question_path(question) }
@@ -16,8 +15,8 @@ feature 'User can watch question and answers to it' do
     end
 
     scenario 'view a of answers' do
-      expect(page).to have_content answer.body
-      expect(page).to have_content answer1.body
+      expect(page).to have_content answer[0].body
+      expect(page).to have_content answer[1].body
     end
   end
 end
