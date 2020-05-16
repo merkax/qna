a5f7dbf070a5bea72b8e0aa3d8547cc389cfa1a8
merkax <mishabigr@yandex.ru>
2020-05-16 21:41:29 +0300

    fix second assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
+//= link_directory ../stylesheets/custom .css
+//= link_directory ../stylesheets/custom .scss
