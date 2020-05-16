ffe5ed2f45ffdb513768eac4f2a2ac48bec18066
merkax <mishabigr@yandex.ru>
2020-05-16 23:47:09 +0300

    attempt 3

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_directory ../stylesheets .scss
-//= link_directory ../stylesheets/custom .css
-//= link_directory ../stylesheets/custom .scss
